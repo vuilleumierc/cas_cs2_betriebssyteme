95db5a6b974c71fd26087d2a9a7db03c06943132
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2022-03-29 21:29:42 +0200

    Aufgabe Lektion 5

---
+++ week5/interrupt.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import threading
+import time
+
+interrupt_interval=2 # in Sekunden
+
+# ACHTUNG:
+# * `interrupt` ist "Shared State"!
+# * ... und ist *nicht* synchronisiert!
+# * -> das ist *nicht* korrekt!
+#
+interrupt=False
+
+def interrupt_controller_thread():
+  while True:
+    global interrupt
+    time.sleep(interrupt_interval)
+    print("Triggering interrupt")
+    interrupt=True
+
+def cpu_handle_interrupt():
+    global interrupt
+
+    if interrupt == True:
+      print("############## INTERRUPT ###############")
+      # now act on the interrupt...
+      interrupt = False
+
+class CPU():
+    
+    def __init__(self, ram):
+        self.pc = 0
+        self.ram = ram
+
+    def execute(self):
+        while True:
+            if self.pc < len(self.ram):
+                cmd = self.ram[self.pc]
+                print(f"pc: {self.pc} cmd: {cmd}")
+                if cmd == "NOP":
+                    self.pc += 1
+                else:
+                    print(f"Unknown command {cmd}")
+                    self.pc +=1
+            else:
+                self.pc = 0
+            time.sleep(.5)
+            cpu_handle_interrupt()
+
+ram = ["NOP", "NOP", "NOP", "NOP", "NOP", "NOP"]
+cpu = CPU(ram)
+
+cpu_thread = threading.Thread( target = cpu.execute )
+interrupt_thread = threading.Thread( target = interrupt_controller_thread)
+cpu_thread.start()
+interrupt_thread.start()
